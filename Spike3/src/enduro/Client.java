510effd4d6219cc9012895d24f077a1de22c8854
Kasper <kasper_kappe_@hotmail.com>
2015-02-05 16:51:12 +0100

    Added CLient parts

--- Spike3/src/enduro/Client.java
+++ Spike3/src/enduro/Client.java
@@ -1,10 +1,20 @@
 package enduro;
 
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Client extends JFrame {
+	private JLabel startNumberLabel;
+	private JTextField startNumberTF;
+	private JEditorPane results;
 	
 	public Client() {
+		setUp();
+	}
+
+	private void setUp() {
 		
 	}
 
