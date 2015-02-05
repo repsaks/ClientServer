792e80e71e31019c2b0a28387eabfe04b6812a42
Kasper <kasper_kappe_@hotmail.com>
2015-02-05 21:30:55 +0100

    Most of network code done

--- Spike3/src/enduro/Client.java
+++ Spike3/src/enduro/Client.java
@@ -1,10 +1,13 @@
 package enduro;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -15,13 +18,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Client extends JFrame {
+	private BufferedReader in;
+	private PrintWriter out;
+
 	private JLabel startNumberLabel;
 	private JTextField startNumberTF;
 	private JEditorPane results;
 	private JButton register;
 
 	public Client() {
-		
 		initiate();
 		setUp();
 	}
@@ -30,12 +35,13 @@ public class Client extends JFrame {
 		startNumberLabel = new JLabel("Start Number: ");
 		startNumberTF = new JTextField();
 		try {
-			results = new JEditorPane("http://users.student.lth.se/fte10kso/results.html");
+			results = new JEditorPane(
+					"http://users.student.lth.se/fte10kso/results.html");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		register = new JButton("Register");
-		
+
 	}
 
 	private void setUp() {
@@ -65,12 +71,34 @@ public class Client extends JFrame {
 
 	}
 
+	public String sendToServer(String startNumber, long time) throws IOException {
+		// Make connection and initialize streams
+		Socket socket = new Socket("localhost", 9898);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(), true);
+
+		out.println(startNumber);
+		out.println(time);
+
+		// Read Server msg
+		String msg = in.readLine();
+		socket.close();
+		return msg;
+	}
+
 	private class RegisterListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String input = startNumberTF.getText();
 			if (isNumeric(input)) {
+				try {
+					String response = sendToServer(input, System.currentTimeMillis());
+					JOptionPane.showMessageDialog(null, response);
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(null,
+							"Could not connect to server");
+				}
 				startNumberTF.setText("");
 			} else {
 				JOptionPane.showMessageDialog(null, "Must be number");
