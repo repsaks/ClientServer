d751a7f0cda10b238317e53dee0175c0c01cfc1f
Kasper <kasper_kappe_@hotmail.com>
2015-02-05 18:06:41 +0100

    finished non network part of client

--- Spike3/src/enduro/Client.java
+++ Spike3/src/enduro/Client.java
@@ -1,8 +1,10 @@
 package enduro;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -19,33 +21,42 @@ public class Client extends JFrame {
 	private JButton register;
 
 	public Client() {
-		startNumberLabel = new JLabel("Start Number: ");
-		startNumberTF = new JTextField();
-		results = new JEditorPane();
-		register = new JButton("Register");
+		
+		initiate();
 		setUp();
 	}
 
-	private void setUp() {
+	private void initiate() {
+		startNumberLabel = new JLabel("Start Number: ");
+		startNumberTF = new JTextField();
+		try {
+			results = new JEditorPane("http://users.student.lth.se/fte10kso/results.html");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		register = new JButton("Register");
+		
+	}
 
-		setLayout(new GridLayout(2, 1));
+	private void setUp() {
+		setLayout(new BorderLayout());
 		JPanel topRow = new JPanel();
 		topRow.add(startNumberLabel);
 		topRow.add(startNumberTF);
 		topRow.add(register);
-		add(topRow);
-		add(results);
+		add(topRow, BorderLayout.NORTH);
+		add(results, BorderLayout.CENTER);
 
 		startNumberTF.setColumns(20);
 
 		register.addActionListener(new RegisterListener());
 
 		results.setEditable(false);
-		results.setText("Test");
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		pack();
+		setResizable(false);
 		setVisible(true);
 	}
 
@@ -65,7 +76,6 @@ public class Client extends JFrame {
 				JOptionPane.showMessageDialog(null, "Must be number");
 			}
 		}
-
 	}
 
 	public static void main(String[] args) {
